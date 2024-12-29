cb144996b3c450a53207ecdff20fe972eefb8558
Advenure <kkamrantasleem@gmail.com>
2024-12-30 02:38:34 +0500

    Update page.tsx

--- src/app/component/footer/page.tsx
+++ src/app/component/footer/page.tsx
@@ -1,7 +1,7 @@
 import { Facebook, LinkIcon } from "lucide-react";
-import { FaTiktok } from "react-icons/fa";
 
-import { TiSocialTwitter } from "react-icons/ti";
+
+
 
 export default function Footer(){
     return(
@@ -10,9 +10,11 @@ export default function Footer(){
         <h6 className="text-lg text-gray-300">Stay connected with us:</h6>
 
         <ul className="flex flex-wrap justify-center gap-x-8 gap-4 mt-8 mb-12 hover:scale-105 duration-200">
-         <FaTiktok/>
+        
          <Facebook/>
-         <TiSocialTwitter/>
+             <Facebook/>
+             <Facebook/>
+        
          <LinkIcon/>
         </ul>
 
